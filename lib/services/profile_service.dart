3f1aedba2a4bcf6f63674587440909b841008496
KielSitum <yehezkiels.28022005@gmail.com>
2025-12-11 13:58:51 +0700

    Form Verfikasi Petani

--- lib/services/profile_service.dart
+++ lib/services/profile_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ProfileService {
-  final String _baseUrl = "http://127.0.0.1:8000/api"; 
+  final String _baseUrl = "http://10.0.2.2:8000/api"; 
 
   Future<Map<String, dynamic>> updateProfile({
     required String fullName,
